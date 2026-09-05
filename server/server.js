8d616aee49465c2364a73003e98988f63490306e
youssefsameh88 <youssefsamehnazeer@gmail.com>
2026-09-05 22:39:20 +0300

    Allow production frontend in CORS

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://my-form-full-stack-form-and-quiz-pl.vercel.app",
     credentials: true
 }));
 
